9cd8ba71f9507d5992db9cdca6c705cb67bf0317
Facundo244 <facaagus@gmail.com>
2018-08-28 11:11:33 -0300

    Funciones

--- Funciones/main.c
+++ Funciones/main.c
@@ -8,27 +8,49 @@
 //queDevuelve comoseLlama (queRecibe)
 
 void sumar1(void);
+void sumar2(int , int);
 
 int main()
 {
-    sumar1();
+    //sumar1();
+    int numeroUno;
+    int numeroDos;
+
+    printf("Ingrese un numero entero: ");
+    scanf("%d", & numeroUno);
+
+
+    printf("Ingrese un numero entero: ");
+    scanf("%d", & numeroDos);
+
+    sumar2(numeroUno , numeroDos);
+
     return 0;
 }
-void sumar1(void)
+    void sumar2 (int n1 , int  n2)
+{
+    int suma;
+
+    suma = n1+n2;
+
+    printf("La suma es: %d\n", suma);
+
+}
+    void sumar1(void)
 {
     int numeroUno;
     int numeroDos;
     int suma;
 
     printf("Ingrese un numero entero: ");
-    scanf("%d" , & numeroUno);
+    scanf("%d", & numeroUno);
 
 
     printf("Ingrese un numero entero: ");
-    scanf("%d" , & numeroDos);
+    scanf("%d", & numeroDos);
 
     suma = numeroUno+numeroDos;
 
-    printf("La suma es: %d\n" , suma);
+    printf("La suma es: %d\n", suma);
 
 }
