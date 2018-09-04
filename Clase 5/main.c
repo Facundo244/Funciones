865c29ea1a508998cb90d6ddb16ff707d39ff95b
Facundo244 <facaagus@gmail.com>
2018-09-04 10:52:36 -0300

    Clases

---
+++ Clase 5/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#define T 10 ----> todo lo que se ponga con T va a valir 10
+int main()
+{
+   //Vectores indexan en 0
+   //El 0 se llama limite inferior y el ultimo es el limite superior(n-1)
+   //int ejemplo[]---> vector
+   //int ejemplo[5--> cantidad de elementos que se pude guardar]
+
+   //ejemplo[2]= 9; guardar 9 en subindice 2
+   //printf("%d" , vector[2]);
+   //
+   //int vector[5]
+   //int i;
+
+   //for(i=0 ; i<5 ; i++)
+   //{
+   //   printf("Ingrese un numero: ");
+   //    scanf("%d" , &vector[i]);
+   //}
+    //for(i=0 ; i<5 ; i++)
+   //{
+   //   printf("%d\n" , vector[i]);
+   //}
+    //for(i=0 ; i<5 ; i++)
+   //{
+   //   if(vector[i]%2 == 0)----> solo pares
+   //   {
+        //printf("%d\n" , vector[i]);
+   //   }
+   //}
+    //el vector se le puede poner numeros cuando se declara ínt vector[T] = (1 ,2 ,3 ..)
+
+    return 0;
+}
