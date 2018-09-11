bb3fb6134ef27f222e32ed1b161eed406b6a47ea
Facundo <facaagus@gmail.com>
2018-09-10 21:40:52 -0300

    nombre y apellido

--- NombresyApellido/main.c
+++ NombresyApellido/main.c
@@ -3,33 +3,44 @@
 #include <string.h>
 #include <ctype.h>
 
-void inicialesMays(char cadena[] , char fuente[]);
+void inicialesMays(char cadena[]);
 
 int main()
 {
     char nombre[45];
     char apellido[45];
 
+    printf("Ingrese su nombre: ");
+    fflush(stdin);
+    scanf("%s" , nombre);
 
-    inicialesMays( nombre, "facundo" );
-    inicialesMays( apellido, "morcillo" );
+
+    printf("Ingrese su apellido: ");
+    fflush(stdin);
+    scanf("%s" , apellido);
+
+    inicialesMays(apellido);
+    inicialesMays(nombre);
 
     printf("%s , %s" , nombre , apellido);
 
-
     return 0;
 }
-void inicialesMays( char cadena[], char fuente[] )
+void inicialesMays( char cadena[])
 {
-    char caracter = ' ';
-    int i;
-
-    for (i=0; fuente[i] != '\0'; i++) {
-        if (caracter == ' ' && fuente[i] >= 'a' && fuente[i] <= 'z')
-            cadena[i] = toupper( fuente[i] );
-        else
-            cadena[i] = fuente[i];
-        caracter = fuente[i];
+   int i,aux;
+    for (i=0;i<100;i++){
+        if (i==0 && (isspace(i)==0)){
+            aux =toupper(cadena[i]);
+            cadena[i]=aux;
+        }
+        else if(isspace(cadena[i]) == 0 && isspace(cadena[i-1])==1){
+            aux = toupper(cadena[i]);
+            cadena[i] = aux;
+        }
+        else{
+            aux = tolower(cadena[i]);
+            cadena[i]=aux;
+        }
     }
-    cadena[i] = '\0';
 }
