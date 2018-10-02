d7c6f010ea5082ea755406bd694f047d3163a932
Facundo244 <36156869+Facundo244@users.noreply.github.com>
2018-10-02 12:08:21 -0300

    Add files via upload

---
+++ Facundo Morcillo/Evaluacion/Evaluacion/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Biblioteca.h"
+
+
+int main()
+{
+    sFilm listadoFilm[TAM];
+    inicializarArrayIlogicamente(listadoFilm);
+    menu(listadoFilm);
+}
